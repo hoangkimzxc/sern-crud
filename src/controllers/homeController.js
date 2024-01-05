6305a1637baee85c57d52144fc02e3efe46130cb
hoangnk <hoangkimzxc@gmail.com>
2024-01-05 14:00:25 +0700

    Sử Dụng ORM Để Xóa Dữ liệu (Delete) - Phần 4

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -24,10 +24,6 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser()
-    console.log('----------------');
-    console.log(data);
-    console.log('----------------');
-
     return res.render('displayCRUD.ejs', {
         dataTable: data
     })
@@ -49,11 +45,23 @@ let putCRUD = async (req, res) => {
     return res.render('displayCRUD.ejs', { dataTable: allUsers })
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id)
+        return res.send('Delete the user succeed!')
+    }
+    else {
+        return res.send('User not found')
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
-    putCRUD: putCRUD
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
